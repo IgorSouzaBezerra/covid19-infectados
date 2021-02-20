7166dceddd97803dd980d6210960e3192682ccce
IgorSouzaBezerra <igor.souza1.bezerra@gmail.com>
2021-02-20 11:23:34 -0300

    project complete

---
+++ Controllers/InfectadoController.cs
@@ -0,0 +1,44 @@
+﻿using ApiEMongo.Data.Collections;
+using ApiEMongo.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace ApiEMongo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public IActionResult SavarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+            _infectadosCollection.InsertOne(infectado);
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public IActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+            return Ok(infectados);
+        }
+
+        // [HttpGet]
+        // public IActionResult AtualizarInfectado([FromBody] InfectadoDto dto)
+        // {
+        //     _infectadosCollection.UpdateOne(Builders<Infectado>.Filter.Where);
+        //     var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+        //     return Ok(infectados);
+        // }
+    }
+}
